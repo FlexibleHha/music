21da6a561da021484afd303ae8b3688236325516
FlexibleHha <18037890117@163.com>
2019-05-10 14:06:33 +0800

    轮播组件

---
+++ src/base/slider/slider.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="slider">
+    <div class="slider-group">
+      <slot></slot>
+    </div>
+    <div class="dots">
+      <span class="dot"></span>
+    </div>
+  </div>
+</template>
+
+<script>
+import { addClass } from 'common/js/dom'
+import BScroll from 'better-scroll'
+export default {
+  name: 'slider',
+  data() {
+    return {
+      dots: [],
+      currentPageIndex: 0
+    }
+  },
+  components: {}
+};
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import '~common/stylus/variable'
+  .slider
+    min-height 1px
+    .slider-group
+      position relative
+      overflow hidden
+      .slider-item
+        float left
+        box-sizing border-box
+        overflow hidden
+        text-align center
+</style>
---
+++ src/common/js/dom.js
@@ -0,0 +1,7 @@
+export function hasClass(el, className) {
+  return el.classList.contains(className)
+}
+
+export function addClass(el, className) {
+  return el.classList.add(className)
+}
