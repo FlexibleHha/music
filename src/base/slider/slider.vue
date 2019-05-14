51fc9b2c80c924ecbbd3ab599dbbc582a57d194e
FlexibleHha <18037890117@163.com>
2019-05-14 17:42:37 +0800

    轮播图组件

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="slider">
-    <div class="slider-group">
+  <div class="slider" ref="slider">
+    <div class="slider-group" ref="sliderGroup">
       <slot></slot>
     </div>
     <div class="dots">
-      <span class="dot" v-for="(item, index) in dots" :key="index"></span>
+      <span class="dot" v-for="(item, index) in dots" :key="index" :class="{active: currentPageIndex === index}"></span>
     </div>
   </div>
 </template>
@@ -14,13 +14,94 @@ import { addClass } from 'common/js/dom'
 import BScroll from 'better-scroll'
 export default {
   name: 'slider',
+  props: {
+    loop: {
+      type: Boolean,
+      default: true
+    },
+    autoPlay: {
+      type: Boolean,
+      default: true
+    },
+    interval: {
+      type: Number,
+      default: 4000
+    }
+  },
   data() {
     return {
       dots: [],
       currentPageIndex: 0
     }
   },
-  components: {}
+  methods: {
+    _setSliderWidth(isResize) {
+      this.children = this.$refs.sliderGroup.children
+
+      let width = 0
+      let sliderWidth = this.$refs.slider.clientWidth
+      for (let i = 0; i < this.children.length; i++) {
+        let child = this.children[i]
+        addClass(child, 'slider-item')
+				
+				child.style.width = sliderWidth + 'px'
+				width+=sliderWidth
+      }
+			if (this.loop && !isResize) {
+				width += 2* sliderWidth
+			}
+			this.$refs.sliderGroup.style.width = width + 'px'
+
+    },
+    _initSlider() {
+      this.slider = new BScroll(this.$refs.slider,{
+        scrollX: true,
+        scrollY: false,
+        momentum: false,
+        snap: {
+          loop: this.loop,
+          threshold: 0.3,
+          speed: 400
+        }
+      })
+      this.slider.on('scrollEnd', this._onscrollEnd)
+			this.slider.on('touchend', () => {
+				if (this.autoPlay) {
+					this._play()
+				}
+			})
+    },
+		_initDots() {
+			this.dots = new Array(this.children.length)
+		},
+    _onscrollEnd() {
+      let pageIndex = this.slider.getCurrentPage().pageX // 返回当前的页码
+      this.currentPageIndex = pageIndex
+      if (this.autoPlay) {
+				this._play()
+      }
+    },
+    _play() {
+			clearTimeout(timer)
+      this.timer = setTimeout(() => {
+        this.slider.next()
+      }, this.interval)
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this._setSliderWidth()
+			this._initDots()
+      this._initSlider()
+    })
+		
+		window.addEventListener('resize', () => {
+			if (!this.seller || !this.slider.enabled) {
+				return
+			}
+			
+		})
+  }
 };
 </script>
 
@@ -31,6 +112,7 @@ export default {
     .slider-group
       position relative
       overflow hidden
+      white-space nowrap
       .slider-item
         float left
         box-sizing border-box
@@ -41,4 +123,26 @@ export default {
           width 100%
           overflow hidden
           text-decoration none 
+				img
+					display block
+					width 100%
+		.dots
+			position absolute
+			right 0
+			left 0
+			bottom 12px
+			transform translate(1px)
+			text-align center
+			font-size 0
+			.dot
+				display inline-block
+				margin 0 4px 
+				width 8px
+				height 8px
+				border-radius 50%
+				background $color-text-l
+				&.active
+					width 20px
+					border-radius 5px
+					background $color-text-ll
 </style>
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,83 +1,90 @@
 <template>
-  <div class="recommend" ref="recommend">
-    <scroll>
-      <div class="slide-wrapper" v-if="recommends.length">
-        <div class="slider-content">
-          <slider>
-            <div v-for="(item, index) in recommends" :key="index">
-              <a :href="item.linkUrl">
-                <img :src="item.picUrl">
-              </a>
-            </div>
-          </slider>
-        </div>
-      </div>
-      <div class="recommend-list">
-        <h1 class="list-title">热门歌单推荐</h1>
-        <ul></ul>
-      </div>
-    </scroll>
-  </div>
+	<div class="recommend" ref="recommend">
+		<div class="recommend-content">
+			<!--轮播 -->
+			<div class="slider-wrapper" v-if="recommends.length">
+				<div class="slider-content">
+					<slider>
+						<div v-for="(item, index) in recommends" :key="index">
+							<a :href="item.linkUrl">
+								<img :src="item.picUrl" />
+							</a>
+						</div>
+					</slider>
+				</div>
+			</div>
+			<!-- 推荐列表 -->
+			<div class="recommend-list">
+				<h1 class="list-title">热门歌单推荐</h1>
+				<ul></ul>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-import { getRecommend } from 'api/recommend'
-import { ERR_OK } from 'api/config'
-import Slider from 'base/slider/slider'
-import Scroll from 'base/scroll/scroll'
-export default {
-  data() {
-    return {
-      recommends: [],
-      discList: []
-    };
-  },
-  created() {
-    this._getRecommend();
-  },
-  methods: {
-    _getRecommend() {
-      getRecommend().then(res => {
-        if (res.code === ERR_OK) {
-          this.recommends = res.data.slider;
-          console.log(this.recommends)
-        }
-      });
-    }
-  },
-  components: {
-    Slider,
-    Scroll
-  }
-};
+	import {
+		getRecommend
+	} from 'api/recommend'
+	import {
+		ERR_OK
+	} from 'api/config'
+	import Slider from 'base/slider/slider'
+	// import Scroll from 'base/scroll/scroll'
+	export default {
+		data() {
+			return {
+				recommends: [],
+				discList: []
+			};
+		},
+		created() {
+			this._getRecommend();
+		},
+		methods: {
+			_getRecommend() {
+				getRecommend().then(res => {
+					if (res.code === ERR_OK) {
+						this.recommends = res.data.slider;
+						console.log(this.recommends)
+					}
+				});
+			}
+		},
+		components: {
+			Slider,
+			// Scroll
+		}
+	};
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-@import '~common/stylus/index';
-
-.recommend {
-  position: fixed;
-  top: 88px;
-  width: 100%;
-  .recommend-content
-    height 100%
-    over
-  .slide-wrapper {
-    position: relative;
-    width: 100%;
-    height: 0;
-    padding-top: 40%;
-    overflow: hidden;
-  }
-
-  .recommend-list {
-    .list-title {
-      height: 65px;
-      line-height: 65px;
-      text-align: center;
-      font-size: $font-size-medium;
-      color: $color-theme;
-    }
-  }
-}
+	@import '~common/stylus/variable'
+	.recommend
+		position fixed
+		width 100%
+		top 88px
+		bottom 0
+		.recommend-content
+			height 100%
+			overflow hidden
+			.slider-wrapper
+				position relative
+				width 100%
+				height 0
+				padding-top 40%
+				overflow hidden
+				.slider-content
+					position absolute
+					top 0
+					left 0
+					width 100%
+					height 100%
+			.recommend-list
+				.list-title
+					height 65px
+					line-height 65px
+					text-align center
+					font-size $font-size-medium
+					color $color-theme
 </style>
