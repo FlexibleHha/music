7bfee82e1b14e3cdbdbf417e66f21d8466c76539
FlexibleHha <18037890117@163.com>
2019-05-15 18:26:59 +0800

    有bug回去写

---
+++ src/base/listview/listview.vue
@@ -0,0 +1,187 @@
+<template>
+	<scroll
+		@scroll="scroll"
+		class="listview"
+		ref="listview"
+		:data="data"
+		:probe-type="probeType"
+		:listen-scroll="listenScroll"
+	>
+		<ul>
+			<li class="list-group" v-for="(group, index) in data" :key="index" ref="listGroup">
+				<h2 class="list-group-title">{{group.title}}</h2>
+				<ul>
+					<li class="list-group-item" v-for="(item, index) in group.items" :key="index">
+						<img v-lazy="item.avatar" class="avatar" alt="">
+						<span class="name">{{item.name}}</span>
+					</li>
+				</ul>
+			</li>
+		</ul>
+		<div class="list-shortcut">
+			<ul>
+				<li class="item" v-for="(item, index) in shortcutList" :key="index">{{item}}</li>
+			</ul>
+		</div>
+	</scroll>
+</template>
+
+<script>
+	import Scroll from 'base/scroll/scroll'
+	import loading from 'base/loading/loading'
+	
+	const TITLE_HEIGHT = 30
+	const ANCHOR_HEIGHT = 18
+	export default {
+		props: {
+			data: {
+				type: Array,
+				default() {
+					return []
+				}
+			}
+		},
+		data() {
+			return {
+				scrollY: -1,
+				currentIndex: 0,
+				diff: -1
+			}
+		},
+		created() {
+			this.probeType = 3
+			this.listenScroll = true
+			this.touch = {}
+			this.listHeight = []
+		},
+		methods: {
+			scroll (pos) {
+				 this.scrollY = pos.y
+			},
+			_calculateHeight() {
+				this.listHeight = []
+				const list = this.$refs.listGroup
+				let height = 0
+				this.listHeight.push(height)
+				for (let i = 0; i < list.length; i++) {
+					let item = list[i]
+					height += item.clientHeight
+					this.listHeight.push(height)
+				}
+			},
+			_scrollTo(index) {
+				if (!index && index !== 0) {
+					return
+				}
+				if (index < 0) {
+					index = 0
+				} else if (index > this.listHeight.length - 2) {
+					index = this.listHeight.length - 2
+				}
+				this.$refs.listview,scrollElement(this.$refs.listGroup[index], 0)
+				this.scrollY = this.$refs.listview.scroll.y
+			}
+		},
+		computed: {
+			shortcutList () {
+				return this.data.map((group) => {
+					return group.title.substr(0, 1)
+				})
+			},
+			// title吸顶
+			fixedTitle() {
+				if (this.scrollY > 0) {
+					return ''
+				}
+				return this.data[this.currentIndex] ? this.data[this.currentIndex].title : ''
+			}
+		}, 
+		watch: {
+			data() {
+				setTimeout(() => {
+					this._calculateHeight()
+				}, 20)
+			},
+			scrollY(newY) {
+				const listHeight = this.listHeight
+				// 当滚动到顶部 newY>0
+				if (newY > 0) {
+					this.currentIndex = 0
+					return
+				}
+				// 在中间部分滚动
+				for (let i = 0; i < listHeight.length - 1; i++) {
+					let height1 = listHeight[i]
+					let height2 = listHeight[i+1]
+					if (-newY >= height1 && -newY < height2) {
+						this.currentIndex = i
+						this.diff = height2 + newY
+						return
+					}
+				}
+				this.currentIndex = listHeight.length - 2
+			},
+			diff(newVal) {
+				let fixedTop = (newVal > 0 && newVal < TITLE_HEIGHT) ? newVal - TITLE_HEIGHT : 0
+				if (this.fixedTop === fixedTop) {
+					return 
+				}
+				this.fixedTop = fixedTop
+				this.$refs.fixed.style.transform = `translate3d(0,${fixedTop}px,0)`
+			}
+		},
+		components: {
+			Scroll
+		}
+	}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+	 @import "~common/stylus/variable"
+	 .listview
+			position relative
+			width 100%
+			height 100%
+			overflow hiddden
+			background: $color-background
+			.list-group
+				padding-bottom 30px
+				.list-group-title
+					height 30px
+					line-height 30px
+					padding-left 20px
+					font-size: $font-size-small
+					color: $color-text-l
+					background: $color-highlight-background
+				.list-group-item
+					display flex
+					align-items center
+					padding 20px 0 0 30px
+					.avatar
+						width 50px
+						height 50px
+						border-radius 50%
+					.name
+						margin-left 20px
+						color: $color-text-l
+						font-size: $font-size-medium
+			.list-shortcut
+				position absolute
+				z-index 30
+				right 0
+				top 50%
+				transform translateY(-50%)
+				width 20px
+				padding 20px 0
+				border-radius 10px
+				text-align center
+				background: $color-background-d
+				font-family: Helvetica
+				.item
+					padding 3px
+					line-height: 1
+					color: $color-text-l
+					font-size: $font-size-small
+					&.current
+					  color: $color-theme
+</style>
