5aae6b646517b3ecf46c80a94d251064aa609f87
--global <--global>
2019-08-21 17:01:32 +0800

    add rank search

---
+++ src/base/confirm/confirm.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  components: {}
+};
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus"></style>
--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -1,22 +1,55 @@
 <template>
- <div>
-   i am search
- </div>
+  <div class="search">
+    <div class="search-box-wrapper"></div>
+    <div class="shortcut-wrapper">
+      <scroll>
+        <div>
+          <div class="hot-key">
+            <h1 class="title">热门搜索</h1>
+            <ul>
+              <li class="item">
+                <span>{{}}</span>
+              </li>
+            </ul>
+          </div>
+          <div class="search-history">
+            <h1 class="title">
+              <span class="text"></span>
+              <span>
+                <i class="icon-clear"></i>
+              </span>
+            </h1>
+            <search-list></search-list>
+          </div>
+        </div>
+      </scroll>
+    </div>
+    <div class="search-result">
+      <suggest></suggest>
+    </div>
+    <confirm ref="confirm"></confirm>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
+import SearchBox from "base/search-box/search-box";
+import SearchList from "base/search-list/search-list";
+import Scroll from "base/scroll/scroll";
+import Confirm from "base/confirm/confirm";
+import Suggest from "components/suggest/suggest";
 export default {
- data() {
- return {
-
+  data() {
+    return {};
+  },
+  components: {
+    SearchBox,
+    SearchList,
+    Scroll,
+    Confirm,
+    Suggest
   }
- },
- components: {
-
- }
-}
+};
 </script>
 
-<style scoped lang="stylus" rel="stylesheet/stylus">
-
-</style>
+<style scoped lang="stylus" rel="stylesheet/stylus"></style>
