4452e744c9b58cd476a219b750fdc133bdb98cdf
--global <--global>
2019-08-22 14:18:58 +0800

    search 数据

--- src/base/search-list/search-list.vue
+++ src/base/search-list/search-list.vue
@@ -1,13 +1,34 @@
 <template>
-  <div></div>
+  <div class="search-list">
+    <transition-group tag="ul" name="list">
+      <li class="search-item" @click="selectItem(item)" v-for="item in searches" :key="item">
+        <span class="text">{{item}}</span>
+        <span class="icon" @click.stop="deleteOne(item)">
+          <i class="icon-delete"></i>
+        </span>
+      </li>
+    </transition-group>
+  </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {};
+  props: {
+    searches: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
   },
-  components: {}
+  methods: {
+    selectItem(item) {
+      this.$emit("select", item);
+    },
+    deleteOne(item) {
+      this.$emit("delete", item);
+    }
+  }
 };
 </script>
 
--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -3,25 +3,30 @@
     <div class="search-box-wrapper">
       <search-box ref="searchBox" @query="onQueryChange"></search-box>
     </div>
-    <div class="shortcut-wrapper">
-      <scroll>
+    <div class="shortcut-wrapper" ref="shortcutWrapper">
+      <scroll class="shortcut" ref="shortcut">
         <div>
           <div class="hot-key">
             <h1 class="title">热门搜索</h1>
             <ul>
-              <li class="item" v-for="(item, index) in hotKey" :key="index">
+              <li
+                @click="addQuery(item.k)"
+                class="item"
+                v-for="(item, index) in hotKey"
+                :key="index"
+              >
                 <span>{{item.k}}</span>
               </li>
             </ul>
           </div>
           <div class="search-history">
             <h1 class="title">
-              <span class="text"></span>
-              <span>
+              <span class="text">搜索历史</span>
+              <span @click="showConfirm" class="clear">
                 <i class="icon-clear"></i>
               </span>
             </h1>
-            <search-list></search-list>
+            <search-list :searches="searchHistory"></search-list>
           </div>
         </div>
       </scroll>
@@ -48,13 +53,19 @@ export default {
       hotKey: []
     };
   },
+  created() {
+    this._getHostKey();
+  },
   methods: {
     _getHostKey() {
       getHotKey().then(res => {
         if (res.code === ERR_OK) {
-          this.hotKey = res.data.hotKey.slice(0, 10);
+          this.hotKey = res.data.hotkey.slice(0, 10);
         }
       });
+    },
+    showConfirm() {
+      this.$refs.confirm.show();
     },
     onQueryChange() {}
   },
@@ -77,9 +88,62 @@ export default {
     margin: 20px;
   }
 
-  .short-wrapper {
+  .shortcut-wrapper {
     position: fixed;
     top: 178px;
+    bottom: 0;
+    width: 100%;
+
+    .shortcut {
+      height: 100%;
+      overflow: hidden;
+
+      .hot-key {
+        margin: 0 20px 20px 20px;
+
+        .title {
+          margin-bottom: 20px;
+          font-size: $font-size-medium;
+          color: $color-text-l;
+        }
+
+        .item {
+          display: inline-block;
+          padding: 5px 10px;
+          margin: 0 20px 10px 0;
+          border-radius: 6px;
+          background: $color-highlight-background;
+          font-size: $font-size-medium;
+          color: $color-text-d;
+        }
+      }
+
+      .search-history {
+        position: relative;
+        margin: 0 20px;
+
+        .title {
+          display: flex;
+          align-items: center;
+          height: 40px;
+          font-size: $font-size-medium;
+          color: $color-text-l;
+
+          .text {
+            flex: 1;
+          }
+
+          .clear {
+            extend-click();
+
+            .icon-clear {
+              font-size: $font-size-medium;
+              color: $color-text-d;
+            }
+          }
+        }
+      }
+    }
   }
 }
 </style>
